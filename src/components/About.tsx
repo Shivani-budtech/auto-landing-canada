811a5d9c4b84c51d070e68d3f5ba8e63ac7ca026
Ayush Yadav <65528804+ayushyadav2001@users.noreply.github.com>
2024-10-22 19:07:58 +0530

    about counter changes

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -2,15 +2,17 @@ import React from 'react';
 import InternalHeader from './InternalHeader/index.tsx';
 import AboutFeatures from './AboutFeatures/index.tsx';
 import AboutUs from './AboutUs/index.tsx';
+import CounterSec from './CounterSec/index.tsx';
 import AboutBanner from './AboutBanner/index.tsx';
 import Testimonials from './Testimonials/index.tsx';
 
 function About() {
     return (
-        <div>
+        <div className='about-page'>
             <InternalHeader />
             <AboutFeatures />
             <AboutUs />
+            <CounterSec />
             <AboutBanner />
             <Testimonials />
         </div>
---
+++ src/components/CounterSec/index.tsx
@@ -0,0 +1,45 @@
+import React from 'react';
+import './index.css';
+function CounterSec() {
+
+    return (
+        <section className="counter-sec">
+            <div className='counter-row'>
+                <div className='counter'>
+                    <h1>
+                        +50 k
+                    </h1>
+                    <p>
+                        Cars Sold
+                    </p>
+                </div>
+                <div className='counter'>
+                    <h1>
+                        97%
+                    </h1>
+                    <p>
+                        First Call Resolution Rate
+                    </p>
+                </div>
+                <div className='counter'>
+                    <h1>
+                        100%
+                    </h1>
+                    <p>
+                        Satisfied Customer
+                    </p>
+                </div>
+                <div className='counter'>
+                    <h1>
+                        +100
+                    </h1>
+                    <p>
+                        Experts involved
+                    </p>
+                </div>
+            </div>
+        </section>
+    );
+}
+
+export default CounterSec;
