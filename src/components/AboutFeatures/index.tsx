b0db905bc3e294dd85b99268a5cbc1d7b3e072ec
Ayush Yadav <65528804+ayushyadav2001@users.noreply.github.com>
2024-10-30 17:28:22 +0530

    about page responsive

--- src/components/AboutFeatures/index.tsx
+++ src/components/AboutFeatures/index.tsx
@@ -25,7 +25,7 @@ function AboutFeatures() {
         },
     ];
     return (
-        <section className="why-choose about-features">
+        <section className="about-features">
             <div className="why-choose-row">
                 <img src={require("./dotted-bg.png")} className="first-img" />
                 <img src={require("./round.png")} className="last-img" />
