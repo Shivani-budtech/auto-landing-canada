6f7d2e16b8546ffbb4365ba589111d5191b564a9
Ayush Yadav <65528804+ayushyadav2001@users.noreply.github.com>
2025-02-04 17:12:21 +0530

    Final build

--- src/components/Constant.tsx
+++ src/components/Constant.tsx
@@ -1,3 +1,3 @@
-export const API_URL = 'https://techbv.in/alc-app/admin/api/';
-export const BACKEND_URL = 'https://techbv.in/alc-app/admin/';
+export const API_URL = 'https://autolendingcanada.ca/admin/api/';
+export const BACKEND_URL = 'https://autolendingcanada.ca/admin/';
 export const VAPID_KEY = 'AFtmJL1nkJR2P6WK8OrRNB9snzFuvkeAVQ1eNVmXkrU';
