0302132fca3e4e96cd7ada9cc706142b52768053
Ayush Yadav <65528804+ayushyadav2001@users.noreply.github.com>
2025-02-05 10:35:46 +0530

    Co signer address

--- src/components/ApplySteppers/CoSignerHomeAddress.tsx
+++ src/components/ApplySteppers/CoSignerHomeAddress.tsx
@@ -1,9 +1,18 @@
 import React, { useState } from 'react';
 import { Link } from "react-router-dom";
+import Autocomplete from "react-google-autocomplete";
 
 const CoSignerHomeAddress = ({ formData, setFormData, setalcStep }) => {
     const [isApproved, setIsApproved] = useState("pending");
     const [hasError, setHasError] = useState(0);
+
+    const handlePlaceSelected = (place) => {
+        console.log("Selected Place:", place);
+        const address = place.formatted_address;
+        console.log("Address:", address);
+        setHasError(0);
+        setFormData({ ...formData, co_signer_address: address });
+    };
 
     const checkForApproval = () => {
         if (formData.co_signer_address === "") {
@@ -41,7 +50,19 @@ const CoSignerHomeAddress = ({ formData, setFormData, setalcStep }) => {
                 <div className='stepper-text-input'>
                     <span className="input-guide" style={{ marginLeft: "15px" }}>Home Address</span>
                     <span className='text-input'>
-                        <input type='text' className='' value={formData.co_signer_address} name="co_signer_address" onChange={handleChange} />
+                        <Autocomplete
+                            apiKey="AIzaSyDisxeuwxGC2xoZwvUa1VPiOBzVBTNG2TA"
+                            onPlaceSelected={handlePlaceSelected}
+                            options={{
+                                types: ["geocode"],
+                                componentRestrictions: { country: "ca" },
+                            }}
+                            placeholder="Search for an address"
+                            className="autocomplete-input"
+                            value={formData.co_signer_address}
+                            onChange={handleChange}
+                        />
+                        {/* <input type='text' className='' value={formData.co_signer_address} name="co_signer_address" onChange={handleChange} /> */}
                     </span>
                 </div>
                 <div className='stepper-btn'>
@@ -71,7 +92,7 @@ const CoSignerHomeAddress = ({ formData, setFormData, setalcStep }) => {
                         </div>
                         <div className="approve-pop-address">
                             You Qualify for Pre-Approval in:
-                            <h1>Hamilton, Ontario</h1>
+                                <h1>{formData.co_signer_address }</h1>
                         </div>
                         <div className="approve-pop-content">
                             <p>
