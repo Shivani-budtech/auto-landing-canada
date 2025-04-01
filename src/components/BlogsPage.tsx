ac8c2de024914d67bbac04d6354a40c0fcaf9d39
Shivani-budtech <shivani.budtech@gmail.com>
2025-04-01 10:32:34 +0530

    blog page marketing issue

--- src/components/BlogsPage.tsx
+++ src/components/BlogsPage.tsx
@@ -1,13 +1,36 @@
 import React from 'react';
 import InternalHeader from './InternalHeader/index.tsx';
 import AllBlogsNew from './Blogs/index.tsx';
+import { Helmet } from "react-helmet-async";
 
 function BlogsPage() {
     return (
-        <section className='blogs-page-new'>
-            <InternalHeader />
-            <AllBlogsNew />
-        </section>
+        <>
+            <Helmet>
+                <meta charSet="UTF-8" />
+                <title>Blog - Car Financing, Car Loan Eligibility, Interest Rates & More!</title>
+                <meta 
+                    name="description" 
+                    content="Learn about car loan eligibility, interest rates, and find the best deals at Auto Lending Canada. Get expert tips and insights for a smooth car financing experience" 
+                />
+                <meta name="robots" content="index, follow" />
+                <link rel="canonical" href="https://autolendingcanada.ca/blogs" />
+
+                {/* Open Graph Metadata for Social Sharing */}
+                <meta property="og:type" content="website" />
+                <meta property="og:site_name" content="Auto Lending Canada" />
+                <meta property="og:title" content="Blog - Car Financing, Car Loan Eligibility, Interest Rates & More!" />
+                <meta 
+                    property="og:description" 
+                    content="Learn about car loan eligibility, interest rates, and find the best deals at Auto Lending Canada. Get expert tips and insights for a smooth car financing experience" 
+                />
+                <meta property="og:url" content="https://autolendingcanada.ca/blogs" />
+            </Helmet>
+            <section className='blogs-page-new'>
+                <InternalHeader />
+                <AllBlogsNew />
+            </section>
+        </>
     );
 }
 
